fe01acfe70528e631812c99a982150e9f1794254
innocent.anigbo <innocent.anigbo@yahoo.com>
2018-01-20 15:41:10 +0000

    testing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World, for my face 2 face interview, Innocent2');
+  res.send('Hello World, for my face 2 face interview, Innocent');
 });
 
 var server = app.listen(3000, function () {
