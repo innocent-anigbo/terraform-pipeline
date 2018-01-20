baf1bf5a370a0925d35062377f15a1c0a621c6ed
innocent.anigbo <innocent.anigbo@yahoo.com>
2018-01-20 22:40:42 +0000

    test

--- terraform-code/myapp.tf
+++ terraform-code/myapp.tf
@@ -44,12 +44,11 @@ resource "aws_elb" "myapp-elb" {
 }
 
 resource "aws_ecs_service" "myapp-service" {
-  count = "${var.MYAPP_SERVICE_ENABLE}"
   name = "${var.MYAPP_VERSION}"
   deployment_minimum_healthy_percent = 50
   cluster = "${aws_ecs_cluster.example-cluster.id}"
   task_definition = "${aws_ecs_task_definition.myapp-task-definition.arn}"
-  desired_count = 2
+  desired_count = 4
   iam_role = "${aws_iam_role.ecs-service-role.arn}"
   depends_on = ["aws_iam_policy_attachment.ecs-service-attach1"]
 
