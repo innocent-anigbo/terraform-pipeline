cb4bff33e2ffed3b53c830515c565fcc2ce4cfeb
innocent.anigbo <innocent.anigbo@yahoo.com>
2018-01-21 12:45:58 +0000

    testing

--- terraform-code/myapp.tf
+++ terraform-code/myapp.tf
@@ -48,7 +48,7 @@ resource "aws_ecs_service" "myapp-service" {
   deployment_minimum_healthy_percent = 50
   cluster = "${aws_ecs_cluster.example-cluster.id}"
   task_definition = "${aws_ecs_task_definition.myapp-task-definition.arn}"
-  desired_count = 4
+  desired_count = 2
   iam_role = "${aws_iam_role.ecs-service-role.arn}"
   depends_on = ["aws_iam_policy_attachment.ecs-service-attach1"]
 
